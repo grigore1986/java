4b08b3ca9639833ceb353d1c016c6992029d16a7
LAPTOP-IC3K3REC\borta <borta.grigore@gmail.com>
2023-02-11 15:39:40 +0200

    make enum weekday is holiday

--- Weekday/src/Main.java
+++ Weekday/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Weekday weekday = new Weekday();
 
 
-        System.out.println(Weekday.MONDAY.isWeekday());
+
+        System.out.println(Weekday.SUNDAY.isWeekDay());
     }
 }
--- Weekday/src/Weekday.java
+++ Weekday/src/Weekday.java
@@ -1,13 +1,14 @@
 public enum Weekday {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
-     public boolean isWeekDay() {
-         return this != SATURDAY && this != SUNDAY;
-     }
+    public boolean isWeekDay() {
+        return this != SATURDAY && this != SUNDAY;
+    }
     public boolean isHoliday() {
-        return !isWeekday();
+        return isWeekDay();
     }
 
 
-
-
 }
+
+
+
