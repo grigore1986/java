843287505ca9c21873d606181ede1e2f4994bdcc
LAPTOP-IC3K3REC\borta <borta.grigore@gmail.com>
2023-02-17 17:35:32 +0200

    Această clasă are 4 membri privați, fiecare cu propriul său tip de date: un model de tip String, o descriere de tip String, o cantitate de tip int și un preț de tip double. Constructorul clasei primește aceste 4 parametri și îi atribuie fiecăruia dintre ei valoarea corespunzătoare în instanța curentă a clasei. În plus, există metodele getter/setter pentru fiecare membru, care permit accesul la membrii privați ai clasei din exteriorul acesteia, menținând în același timp encapsularea clasei. De asemenea, clasa are o metodă getAmount care calculează și întoarce suma facturei, prin înmulțirea cantității și prețului.

--- Invoice/src/Invoice.java
+++ Invoice/src/Invoice.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Invoice {
+public class Invoice {
+    private String model; // modelul dispozitivului
+    private String description; // descrierea produsului
+    private int quantity; // cantitatea de produse achiziționate
+    private double price; // prețul pentru un produs
+
+    // Constructorul clasei Invoice inițializează atributele cu valorile primite ca argumente
+    public Invoice(String model, String description, int quantity, double price) {
+        this.model = model;
+        this.description = description;
+        this.setQuantity(quantity);  // inițializarea cantității prin apelarea metodei setQuantity()
+        this.setPrice(price);        // inițializarea prețului prin apelarea metodei setPrice()
+    }
+
+    // Metodele getter și setter pentru fiecare atribut al clasei
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        // Se asigură că valoarea cantității este pozitivă sau zero
+        this.quantity = quantity < 0 ? 0 : quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        // Se asigură că valoarea prețului este pozitivă sau zero
+        this.price = price < 0.0 ? 0.0 : price;
+    }
+
+    // Metoda getAmount calculează și returnează suma facturei ca un double
+    public double getAmount() {
+        return this.price * this.quantity;
+    }
 }
