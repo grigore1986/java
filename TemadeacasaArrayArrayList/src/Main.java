e3c8193579f2fe4e7484bb96cdc32825b1b06bce
LAPTOP-IC3K3REC\borta <borta.grigore@gmail.com>
2023-03-05 18:31:55 +0200

    Crearea clasei zoo,animal,lup.....jaguar, crearea listei array in animale cu nume food si in zoo cu nume animal si afisarea lor in consola

--- TemadeacasaArrayArrayList/src/Main.java
+++ TemadeacasaArrayArrayList/src/Main.java
@@ -1,24 +1,32 @@
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Elefant jumbo = new Elefant("Jumbo", 13);// Cream obiectele pentru animale
-        Leu king = new Leu("King", 10);
-        Lup wolf = new Lup("Wolf", 8);
-        Jaguar jaguar = new Jaguar("Jaguar", 6);
-        Zebra dido = new Zebra("Dido", 9);
-        Zoo myzoo = new Zoo();// Cream obiectul pentru Zoo
-        myzoo.addAnimal(jumbo);// Adaugam animalele in Zoo
-        myzoo.addAnimal(king);
-        myzoo.addAnimal(wolf);
-        myzoo.addAnimal(jaguar);
-        myzoo.addAnimal(dido);
-        myzoo.eatAnimal("Frunze");// Hranim animalele cu frunze si carne
-        myzoo.eatAnimal("Carne");
-        List<Animal> animals = myzoo.getAnimal();// Obtinem o lista cu animalele din Zoo
+    public static void main(String[] args) {//AICI SUNT 2 METODE DE REZOLVARE
+        Zoo myzoo = new Zoo();
+        myzoo.addAnimal(new Elefant("Jumbo", 13));
+        myzoo.addAnimal(new Leu("King", 10));
+        myzoo.addAnimal(new Lup("Wolf",8));
+        myzoo.addAnimal(new Jaguar("Jaguar",6));
+        myzoo.addAnimal(new Zebra("Dido", 9));
+
+//        Elefant jumbo = new Elefant("Jumbo", 13);// Cream obiectele pentru animale
+//        Leu king = new Leu("King", 10);
+//        Lup wolf = new Lup("Wolf", 8);
+//        Jaguar jaguar = new Jaguar("Jaguar", 6);
+//        Zebra dido = new Zebra("Dido", 9);
+//        Zoo myzoo = new Zoo();// Cream obiectul pentru Zoo
+//        myzoo.addAnimal(jumbo);// Adaugam animalele in Zoo
+//        myzoo.addAnimal(king);
+//        myzoo.addAnimal(wolf);
+//        myzoo.addAnimal(jaguar);
+//        myzoo.addAnimal(dido);
+//        myzoo.eatAnimal("Frunze");// Hranim animalele cu frunze si carne
+//        myzoo.eatAnimal("Carne");
+//        List<Animal> animals = myzoo.getAnimal();// Obtinem o lista cu animalele din Zoo
 
         // Parcurgem lista de animale si afisam numele si varsta fiecaruia
-        for (Animal animal : animals) {
+       // for (Animal animal : animals) {
+        for (Animal animal : myzoo.getAnimal()) {
             System.out.println("Numele animalului este: " + animal.getName());
             System.out.println("Varsta animalului este: " + animal.getAge());
         // Obtinem lista de alimente ale animalului si o afisam
