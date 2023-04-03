244d5f4696622dfb552915e01f33c5d46db81ff4
LAPTOP-IC3K3REC\borta <borta.grigore@gmail.com>
2023-04-03 18:51:19 +0300

    1. Creați o clasă Person cu trei câmpuri: firstName, lastName și age.Implementați un constructor care va inițializa toate atributele de instanță. Creați două excepții verificate: InvalidNameException și InvalidAgeException. InvalidNameException va fi aruncată atunci când firstName sau lastName au primit valori nule sau String-uri goale, iar  InvalidRangeException va fi aruncată când vârsta primită ca argument la constructor este mai mică ca 0 sau mai mare ca 150. Inițializați mai multe obiecte de tipul clasei Person. Tratați erorile de compilare utilizând regula handle-or-declare.     2. Creați o clasă cu denumirea Numbers care va avea o metodă divideBy cu un parametru de tip int. Adăugați o verificare în interiorul metodei și asigurați-vă că valoarea primită ca argument este diferită de 0. Atunci când metoda primește o valoare egală cu 0, aruncați o excepție de tipul IllegalArgumentException. Instanțiați un obiect de tipul claseiNumbers și apelați metoda divideBy transmițând diferite valori de intrare. Analizați cazul când metoda primește valoarea 0.
        3. Creați o metodă care verifică câte numere divizibile cu 7 există într-un interval dat, de ex:
     int countDivisibleBy7Numbers(int inferiorLimit, int superiorLimit)

--- TekwillAcademyErori/src/DivisibleBy7Counter.java
+++ TekwillAcademyErori/src/DivisibleBy7Counter.java
@@ -1,9 +1,14 @@
-public class  DivisibleBy7Counter {
+public class  DivisibleBy7Counter{
     public int countDivisibleBy7Numbers(int inferiorLimit, int superiorLimit) throws InvalidRangeException {
         if (inferiorLimit > superiorLimit) {
             throw new InvalidRangeException("Limita inferioara este mai mare decat limita superioara");
         }
+        int count = 0;
+        for (int i = inferiorLimit; i <= superiorLimit; i++) {
+            if (i % 7 == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
-
-
