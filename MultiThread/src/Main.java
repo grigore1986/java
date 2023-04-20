42a9047a16139dd2b40dce531390a2005cf8ae0d
LAPTOP-IC3K3REC\borta <borta.grigore@gmail.com>
2023-04-20 14:02:05 +0300

    1. Creați un program care pornește un Thread care va afișa conținutul unei liste     2. Folosiți metoda sleep() pentru a aștepta ca toată lista să fie afișată

--- MultiThread/src/Main.java
+++ MultiThread/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // definim lista de afișat
         List<String>mylist = Arrays.asList("Unu", "Doi", "Trei", "Patru");
-
+        System.out.println(mylist);
         // cream un obiect ListPrinter și îl folosim pentru a crea un Thread
         PrintareaListei<String>printareaListei = new PrintareaListei<>(mylist);
         Thread printareaListeiThread = new Thread(printareaListei);
